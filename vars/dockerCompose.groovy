22d4657ef8054cfed71c347cd022630fd436d1bd
Marc Garcia Sastre <markcial@gmail.com>
2017-03-01 05:54:46 +0100

    Added dockerCompose object

--- vars/dockerCompose.groovy
+++ vars/dockerCompose.groovy
@@ -1,11 +1,6 @@
 class dockerCompose implements Serializable {
   def up(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
+    println body
     println "UP!"
   }
 }
