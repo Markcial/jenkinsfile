f62df475da4359638b6d3a1a29e59750e64ebf91
Marc Garcia Sastre <markcial@gmail.com>
2017-03-01 09:55:28 +0100

    Added object php

--- vars/php.groovy
+++ vars/php.groovy
@@ -1,12 +1,17 @@
 def composer(options) {
-  def sshSocket = new File(env.SSH_AUTH_SOCK)
+
   def args = ['-v ${env.WORKSPACE}:/app']
-  if(sshSocket.exists()) {
-    args = [
-      '-v ${sshSocket.parent()}:${sshSocket.parent()}',
-      '-e SSH_AUTH_SOCK=${sshSocket}'
-    ]
+
+  if (env.SSH_AUTH_SOCK) {
+    def sshSocket = new File(env.SSH_AUTH_SOCK)
+    if(sshSocket.exists()) {
+      args = [
+        '-v ${sshSocket.parent()}:${sshSocket.parent()}',
+        '-e SSH_AUTH_SOCK=${sshSocket}'
+      ]
+    }
   }
+
   echo args
   docker.image('composer/composer').withRun(args, 'install --ignore-platform-reqs')
 }
